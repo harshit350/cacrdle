c8edf1412ec7b9ad04bb133f57760f040565c9b6
harshit350 <74818234+harshit350@users.noreply.github.com>
2022-05-27 15:36:31 +0530

    Update Rope1.js

--- Rope1.js
+++ Rope1.js
@@ -2,7 +2,7 @@ class Rope {
     constructor(body1,body2,offsetX,offsetY)
     {
         this.offsetX=offsetX
-        this,offsetY=offsetY
+        this.offsetY=offsetY
         var options={
             bodyA:body1,
             bodyB:body2,
