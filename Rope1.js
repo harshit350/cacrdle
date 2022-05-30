7cf24d29b8a73b65e1a6aecd2b14714a98c97f04
harshit350 <74818234+harshit350@users.noreply.github.com>
2022-05-30 15:47:53 +0530

    Update Rope1.js

--- Rope1.js
+++ Rope1.js
@@ -8,7 +8,7 @@ class Rope {
             bodyB:body2,
             pointB:{x:this.offsetX,y:this.offsetY}
       }
-   this.rope=constraint.create(options)
+   this.rope=Constraint.create(options)
    World.add(world,this.rope)
    
     }
